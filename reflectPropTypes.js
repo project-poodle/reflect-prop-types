ff585fc12d2445d44d62f57c6de9a3528a47c6ec
Draco Tadpole <37931043+DracoTadpole@users.noreply.github.com>
2021-01-15 21:44:46 -0800

    tweak

--- reflectPropTypes.js
+++ reflectPropTypes.js
@@ -2,7 +2,7 @@
 //We need to ensure that we are using the same react module as the component we are using
 //For this to happen our script can't depend on react, and we must import react from the target node_modules folder
 //import React from 'react'
-const React = require(process.cwd() + "/node_modules/react")
+// const React = require(process.cwd() + "/node_modules/react")
 const PropTypes = require('prop-types')
 
 var originalPropTypes = PropTypes;
@@ -18,7 +18,7 @@ for(var propType in originalPropTypes){
   );
 }
 
-React.PropTypes = newPropTypes
+// React.PropTypes = newPropTypes
 
 //Enforce different scope for each property
 function reflectiveGetter(propType){
